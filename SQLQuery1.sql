57a3b85c19cc073ead957cbc0f44afc571c793ab
ShashiRanjan42 <101126472+ShashiRanjan42@users.noreply.github.com>
2025-04-03 10:22:23 +0530

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,150 +1,102 @@
--- Solution of Assignment 2 
-USE IMDB
+CREATE DATABASE IMDB;
+
+USE IMDB;
 GO
 
--- Write a query to get the age of the Actors in Days(Number of days).
-SELECT Id, Name, DATEDIFF(DAY, DateOfBirth, GETDATE()) As AgeInDays
-FROM Foundation.Actors;
+CREATE SCHEMA Foundation;
+GO
 
--- Write a query to get the list of Actors who have worked with a given producer X.
-SELECT DISTINCT A.Id, A.Name AS ActorName, P.Name AS ProducerName
-FROM Foundation.Actors A
-JOIN Foundation.Movie_Actors MA ON A.Id = MA.ActorId
-JOIN Foundation.Movies M ON MA.MovieId = M.Id
-JOIN Foundation.Producers P ON M.ProducerId = P.Id
-WHERE P.Name = 'Karan Johar';
+CREATE TABLE Foundation.Actors (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    Sex VARCHAR(10) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Bio NVARCHAR(MAX) NOT NULL,
+);
 
---Write a query to get the list of actors who have acted together in two or more movies.
-SELECT A1.Id AS Actor1Id, A1.Name AS Actor1, A2.Id As Actor2Id, A2.Name AS Actor2, COUNT(*) AS MovieCount
-FROM Foundation.Movie_Actors MA1
-JOIN Foundation.Movie_Actors MA2 ON MA1.MovieId = MA2.MovieId AND MA1.ActorId < MA2.ActorId
-JOIN Foundation.Actors A1 ON MA1.ActorId = A1.Id
-JOIN Foundation.Actors A2 ON MA2.ActorId = A2.Id
-GROUP BY A1.Id, A1.Name, A2.Id, A2.Name
-HAVING COUNT(*) >= 2;
-
---Write a query to get the youngest actor.
-SELECT TOP 1 A.Id, A.Name AS ActorName, A.DateOfBirth
-FROM Foundation.Actors A
-ORDER BY A.DateOfBirth DESC
-
---Write a query to get the actors who have never worked together.
-SELECT A1.Id AS Actor1Id, A1.Name AS Actor1Name, 
-       A2.Id AS Actor2Id, A2.Name AS Actor2Name
-FROM Foundation.Actors A1
-JOIN Foundation.Actors A2 ON A1.Id < A2.Id
-WHERE NOT EXISTS (
-    SELECT 1 
-    FROM Foundation.Movie_Actors MA1
-    JOIN Foundation.Movie_Actors MA2 ON MA1.MovieId = MA2.MovieId
-    WHERE MA1.ActorId = A1.Id 
-      AND MA2.ActorId = A2.Id
+CREATE TABLE Foundation.Producers (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(50) NOT NULL,
+    Sex VARCHAR(10) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Bio NVARCHAR(MAX) NOT NULL,
 );
 
 
---Write a query to get the number of movies in each language.
-SELECT Language, COUNT(*) AS MovieCount
-FROM Foundation.Movies
-GROUP BY Language;
+CREATE TABLE Foundation.Movies (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    YearOfRelease INT NOT NULL,
+    Plot NVARCHAR(MAX),
+    Poster NVARCHAR(500),
+    ProducerId INT NOT NULL,
+	CONSTRAINT FK_Movies_Producers FOREIGN KEY (ProducerId) 
+		REFERENCES Foundation.Producers(Id) ON DELETE CASCADE,
+);
 
---Write a query to get me the total profit of all the movies in each language separately.
-SELECT Language,  SUM(CAST(Profit AS BIGINT)) AS TotalProfit
-FROM Foundation.Movies
-GROUP BY Language;
+CREATE TABLE Foundation.Movie_Actors (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    MovieId INT NOT NULL,
+    ActorId INT NOT NULL,
+    CONSTRAINT FK_MovieActors_Movies FOREIGN KEY (MovieId)
+        REFERENCES Foundation.Movies(Id) ON DELETE CASCADE,
+    CONSTRAINT FK_MovieActors_Actors FOREIGN KEY (ActorId)
+        REFERENCES Foundation.Actors(Id) ON DELETE CASCADE,
+);
 
---Write a query to get the total profit of movies which have actor X in each language.
-SELECT M.Language,  SUM(CAST(M.Profit AS BIGINT)) AS TotalProfit
-FROM Foundation.Movies M
-JOIN Foundation.Movie_Actors MA ON M.Id = MA.MovieId
-JOIN Foundation.Actors A ON MA.ActorId = A.Id
-WHERE A.Name = 'Ranbir Kapoor'
-GROUP BY M.Language;
+ALTER TABLE Foundation.Actors
+ADD CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME DEFAULT GETDATE();
 
---Write a query to get the Total profit by year of release and language
-SELECT YEAR(YearOfRelease) AS Year, Language,  SUM(CAST(Profit AS BIGINT)) AS TotalProfit
-FROM Foundation.Movies
-GROUP BY YEAR(YearOfRelease), Language;
+ALTER TABLE Foundation.Producers
+ADD CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME DEFAULT GETDATE();
 
---Write a query to get number of movies in each language produced by each producer
-SELECT P.Id, P.Name AS ProducerName, M.Language, COUNT(*) AS MovieCount
-FROM Foundation.Movies M
-JOIN Foundation.Producers P ON M.ProducerID = P.Id
-GROUP BY P.Id, P.Name, M.Language;
+ALTER TABLE Foundation.Movies
+ADD CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME DEFAULT GETDATE();
 
+ALTER TABLE Foundation.Movie_Actors
+ADD CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME DEFAULT GETDATE();
 
--- Stored Procedure to Insert a Movie
-CREATE PROCEDURE spAddMovie
-    @Name NVARCHAR(100),
-    @YearOfRelease INT,
-    @Plot NVARCHAR(500),
-    @Poster NVARCHAR(500),
-    @ProducerId INT,
-    @ActorIds NVARCHAR(MAX),
-    @Language VARCHAR(50),
-    @Profit INT
-AS
-BEGIN
-	INSERT INTO Foundation.Movies(Name, YearOfRelease, Plot, Poster, ProducerId, Language, Profit, CreatedAt, UpdatedAt)
-    VALUES (@Name, @YearOfRelease, @Plot, @Poster, @ProducerId, @Language, @Profit, GETDATE(), GETDATE());
+ALTER TABLE Foundation.Movies
+ADD Language VARCHAR(50) NOT NULL 
+CONSTRAINT DF_Movies_Language DEFAULT 'Hindi';
 
-	DECLARE @MovieId INT = SCOPE_IDENTITY();
+ALTER TABLE Foundation.Movies
+ADD Profit INT NOT NULL CONSTRAINT DF_Movies_Profit DEFAULT 0;
 
-	INSERT INTO Foundation.Movie_Actors (MovieId, ActorId)
-    SELECT @MovieId, CAST(value AS INT) FROM STRING_SPLIT(@ActorIds, ',');
-END;
+INSERT INTO Foundation.Actors (Name,Sex, DateOfBirth, Bio) VALUES 
+('Ranbir Kapoor', 'Male', '1982-09-28', 'Works in Hindi Movies'),
+('Allu Arjun', 'Male', '1983-04-08', 'Works in South Indian Movies'),
+('Deepika Padukone', 'Female', '1986-01-05', 'Leading actress in Bollywood'),
+('Shah Rukh Khan', 'Male', '1965-11-02', 'King of Bollywood'),
+('Priyanka Chopra', 'Female', '1982-07-18', 'Indian actress and singer');
 
+INSERT INTO Foundation.Producers (Name,Sex, DateOfBirth, Bio) VALUES 
+('Karan Johar', 'Male', '1972-05-25', 'Famous Bollywood producer'),
+('Allu Aravind', 'Male', '1949-01-10', 'South Indian film producer'),
+('Rakesh Roshan', 'Male', '1949-09-06', 'Indian director and producer');
 
--- Stored Procedure to Delete a Movie
-CREATE PROCEDURE spDeleteMovie
-    @MovieId INT
-AS
-BEGIN
-    DELETE FROM Foundation.Movie_Actors WHERE MovieId = @MovieId;
-    DELETE FROM Foundation.Movies WHERE Id = @MovieId;
-END;
-
-
--- Delete Producer
-CREATE PROCEDURE spDeleteProducer
-    @ProducerId INT
-AS
-BEGIN
-    DELETE MA FROM Foundation.Movie_Actors MA 
-	JOIN Foundation.Movies M ON MA.MovieId = M.Id
-    WHERE ProducerId = @ProducerId;
-
-    DELETE FROM Foundation.Movies WHERE ProducerId = @ProducerId;
-    DELETE FROM Foundation.Producers WHERE Id = @ProducerId;
-END;
-
--- Delete an Actor
-CREATE PROCEDURE spDeleteActor
-    @ActorId INT
-AS
-BEGIN
-    DELETE FROM Foundation.Movie_Actors WHERE ActorId = @ActorId;
-    DELETE FROM Foundation.Actors WHERE Id = @ActorId;
-END;
+INSERT INTO Foundation.Movies(Name, YearOfRelease, Plot, Poster, ProducerId, Language, Profit) VALUES 
+('Jai Ho', 2014, 'A man fights against corruption.', 'https://example.com/jaiho.jpg', 1, 'Hindi', 450),
+('Baahubali', 2015, 'A historical epic movie.', 'https://example.com/baahubali.jpg', 2, 'Telugu', 1500),
+('Chennai Express', 2013, 'A comedy-action film.', 'https://example.com/chennai.jpg', 1, 'Hindi', 800),
+('Raees', 2017, 'Crime thriller based on real events.', 'https://example.com/raees.jpg', 3, 'Hindi', 900),
+('Zindagi Na Milegi Dobara', 2011, 'A road trip movie.', 'https://example.com/znmd.jpg', 1, 'Hindi', 700),
+('Kabhi Khushi Kabhie Gham', 2001, 'A family drama.', 'https://example.com/k3g.jpg', 1, 'Hindi', 850);
 
 
+INSERT INTO Foundation.Movie_Actors(MovieId, ActorId) VALUES 
+(1, 1), (1, 3), (1, 4),
+(2, 2), (2, 3),
+(3, 4), (3, 3),
+(4, 4), (4, 3),
+(5, 3), (5, 1), (5, 4),
+(6, 1), (6, 3), (6, 4);
 
-EXEC spAddMovie 
-    @Name = 'ABCD2',
-    @YearOfRelease = 2015,
-    @Plot = 'Hindi Movie',
-    @Poster = 'ABCD_poster.jpg',
-    @ProducerId = 1,
-    @ActorIds = '1,4',
-    @Language = 'Hindi',
-    @Profit = 1000;
-
-EXEC spDeleteMovie @MovieId = 13;
-
-EXEC spDeleteProducer @ProducerId = 1;
-
-EXEC spDeleteActor @ActorId = 101;
-
-SELECT * FROM Foundation.Movies
-SELECT * FROM Foundation.Actors
-SELECT * FROM Foundation.Movie_Actors
-SELECT * FROM Foundation.Producers
+SELECT * FROM Foundation.Actors;
+SELECT * FROM Foundation.Producers;
+SELECT * FROM Foundation.Movies;
+SELECT * FROM Foundation.Movie_Actors;
